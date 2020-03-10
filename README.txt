8a7d6136f93f88203eb181150be88c8f7af8ad2b
0riv3r <ofer.rivlin@cyberark.com>
2020-03-09 19:46:40 -0700

    graphwalker generates offline tests that can be executed

--- README.txt
+++ README.txt
@@ -3,3 +3,19 @@ https://learntocodetogether.com/binary-search-tree-java/
 https://github.com/namvdo/data-structures-and-algorithms-in-java/blob/master/binary-search-tree/Bst.java
 
 https://github.com/microsoft/vscode-java-test/issues/851
+
+generate offline testcase
+=========================
+java -jar graphwalker-cli-4.2.0.jar offline -m C:\workspace\bst\bin\resources\model.json --unvisited --verbose "random(edge_coverage(100))"
+
+# To get the element names only, and also using seed:
+java -jar C:\workspace\bst\lib\graphwalker-cli-4.2.0.jar offline --seed 147945811993279 -m C:\workspace\bst\bin\resources\model.json "random(edge_coverage(100))" | jq '.currentElementName'
+
+# Generate junit source code for the model:
+java -jar C:\workspace\bst\lib\graphwalker-cli-4.2.0.jar source -i C:\workspace\bst\bin\resources\model.json C:\workspace\bst\src\resources\junit.template > C:\workspace\bst\src\test\BstGwTest.java
+
+# Generate a java test souce code:
+java -jar C:\workspace\bst\lib\graphwalker-cli-4.2.0.jar source -i C:\workspace\bst\bin\resources\model.json C:\workspace\bst\src\resources\java.template > C:\workspace\bst\src\test\BstGwTest.java
+
+Run the graphwalker test file from the commandline with enabling assertion:
+PS C:\workspace\bst\bin\test> java -ea BstGwTest
--- src/app/Bst.java
+++ src/app/Bst.java
@@ -1,3 +1,7 @@
+/**
+ * Binary Serach Tree class
+ * @param <T>
+ */
 public class Bst<T extends Comparable<T>> {
     private Node root = null;
     private int nodeCount = 0;
@@ -18,30 +22,26 @@ public class Bst<T extends Comparable<T>> {
     }
 
     private Node insert(Node node, T value) {
-        /** We found the leaf node here, then insert the value to this position. */
+        /** a leaf node, insert the value in this position. */
         if (node == null) {
             node = new Node(null, null, value);
         }
         /**
-         * If the value we pass in is less than the current node's value, then we go to
-         * the left.
+         * If the value is less than the current node's value, choose left.
          */
         else if (value.compareTo(node.element) < 0) {
             node.left = insert(node.left, value);
-
         }
         /**
-         * If the value we pass in is greater than the current node's value, then we go
-         * to the right.
+         * If the value is greater than the current node's value, choose right.
          */
         else if (value.compareTo(node.element) > 0) {
             node.right = insert(node.right, value);
         }
-        /** If the value we pass in already exists in the tree, we return null. */
+        /** If the value already exists in the tree, return null. */
         else if (value.compareTo(node.element) == 0) {
             return null;
         }
-        /** Finally return the node after finishing insertion */
         return node;
 
     }
@@ -53,25 +53,25 @@ public class Bst<T extends Comparable<T>> {
 
     public void display(Node node) {
         if (node != null) {
-            /** recursively calls display method on the left sub-tree */
+            /** recursively call 'display' on the left sub-tree */
             display(node.left);
             /** prints the value at the current node */
             System.out.println("The current node value is " + node.element);
-            /** recursively calls display method for the right subtree. */
+            /** recursively call 'display' on the right subtree. */
             display(node.right);
         }
 
     }
 
     private boolean contains(Node node, T value) {
-        /** returns false if we cannot find the value in the tree. */
+        /** if the value is not found */
         if (node == null)
             return false;
-        /** if two values are equal, found it! And returns true. */
+        /** if the value is found */
         if (value.compareTo(node.element) == 0) {
             return true;
         }
-        /** Otherwise, recursively go back or right to search for the value. */
+        /** Otherwise, continue the search recursively */
         return value.compareTo(node.element) < 0 ? contains(node.left, value) : contains(node.right, value);
 
     }
@@ -80,7 +80,7 @@ public class Bst<T extends Comparable<T>> {
         return contains(root, value);
     }
 
-    /** goes with one direction to the left to the smallest value. */
+    /** return the smallest value (leftmost value) */
     public T smallest(Node node) {
         while (node.left != null) {
             node = node.left;
@@ -88,7 +88,7 @@ public class Bst<T extends Comparable<T>> {
         return node.element;
     }
 
-    /** goes to the right most to find the largest one. */
+    /** return the largest value (rightmost value) */
     public T largest(Node node) {
         while (node.right != null) {
             node = node.right;
